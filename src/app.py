fa78643c130d26579dafc32c3419e3a14ce21232
clisdermar <clisdermar@gmail.com>
2024-04-12 00:44:51 +0000

    5ta modificacion

--- src/app.py
+++ src/app.py
@@ -45,7 +45,7 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
     users = User.query.all()
 
